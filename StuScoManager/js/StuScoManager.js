63d0c6e9322b4201f0a16eb92f6cce89ba89dc9c
gc <gouchao2008@qq.com>
2016-01-01 20:17:29 +0800

    添加教课信息界面

---
+++ StuScoManager/js/StuScoManager.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	$(".teacher_name_list").hide();
+	$(".course_name_list").hide();
+	$(".input_teacher_name").keyup(function(){
+		$(".teacher_name_list").slideDown(300);
+	});
+	$(".input_course_name").keyup(function(){
+		$(".course_name_list").slideDown(300);
+	});
+	$(".input_teacher_name").blur(function(){
+		$(".teacher_name_list").slideUp(300);
+	});
+	$(".input_course_name").blur(function(){
+		$(".course_name_list").slideUp(300);
+	});
+	$(".teacher_name_list button").click(function(){
+		var context = $(this).html();
+		var info = context.split(" _ ");
+		var name = info[0];
+		var id = info[1];
+		$(".input_teacher_name").val(name);
+		$(".input_teacher_id").val(id);
+	});
+	$(".course_name_list button").click(function(){
+		var context = $(this).html();
+		var info = context.split(" _ ");
+		var name = info[0];
+		var id = info[1];
+		$(".input_course_name").val(name);
+		$(".input_course_id").val(id);
+	});
+});
